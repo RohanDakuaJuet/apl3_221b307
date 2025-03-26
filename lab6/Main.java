c6e275109d05e0f34a4ab7a1b309761724f23cd9
221B307 <221B307@juet.co.in>
2025-03-26 15:47:28 +0530

    lab 6

---
+++ lab6/Main.java
@@ -0,0 +1,12 @@
+package lab6;
+
+public class Main {
+    public static void main(String[] args) {
+        Offering a = new Coffee();
+        a =  new Rum(a);
+        a =  new Rum(a);
+        a =  new Rum(a);
+        System.out.println(a.getName() + "\n" + a.getPrice()+" rs");
+    }
+
+}
